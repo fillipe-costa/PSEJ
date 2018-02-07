f0176502cba99d0f948cb7a9759a5dd57042cc75
f <fillipe.costa@gmail.com>
2018-02-07 12:23:37 -0200

    +tabs

--- src/pages/anime/anime.ts
+++ src/pages/anime/anime.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-anime',
+  templateUrl: 'anime.html'
 })
-export class ContactPage {
+export class AnimePage {
 
   constructor(public navCtrl: NavController) {
 
